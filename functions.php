bd936e8290d6a58a36ec60cc32753c4e779e00b4
davideleuterius <davideleuterius@gmail.com>
2019-01-19 12:44:39 -0600

    restructure

--- functions.php
+++ functions.php
@@ -8,30 +8,10 @@
  require_once('functions/cleanup.php');
  require_once('functions/enqueue.php');
 
-/*
-if ( ! function_exists( 'site_setup' ) ) :
-	
-	function site_setup() {
+// disable Gutenberg
 
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'post-thumbnails' );
-		add_image_size( 'site-featured-image', 640, 9999 );
+// disable for posts
+add_filter('use_block_editor_for_post', '__return_false', 10);
 
-		register_nav_menus( array(
-			'main-menu' => esc_html__( 'Header', 'site' ),
-			'footer-menu' => esc_html__( 'Footer', 'site' ),
-			) );
-
-		add_theme_support( 'html5', array(
-			'search-form',
-			'comment-form',
-			'comment-list',
-			'gallery',
-			'caption',
-		) );
-
-	}
-
-endif;
-add_action( 'after_setup_theme', 'site_setup' );
-*/
+// disable for post types
+add_filter('use_block_editor_for_post_type', '__return_false', 10);
