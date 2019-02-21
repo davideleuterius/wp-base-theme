efdee034820f06926794b7db871a99960e7a2201
davideleuterius <davideleuterius@gmail.com>
2019-02-20 18:50:34 -0600

    removing images

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -9,9 +9,8 @@ function site_scripts() {
   wp_enqueue_script( 'jquery' );
   wp_enqueue_script( 'site-js', ADIR . '/js/site.min.js', 'jquery', '1.0.0', TRUE );
   
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) :
-		wp_enqueue_script( 'comment-reply' );
-	endif;
+  if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) :
+    wp_enqueue_script( 'comment-reply' );
+  endif;
 }
 add_action( 'wp_enqueue_scripts', 'site_scripts' );
